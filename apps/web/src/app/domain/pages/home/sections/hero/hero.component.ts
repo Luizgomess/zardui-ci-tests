3bbb7d794fa7a27df14ea1d325cea8e6793688a4
luiz gomes <gomesluiz.dev@gmail.com>
2025-12-25 15:58:05 -0300

    ✨ feat: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- apps/web/src/app/domain/pages/home/sections/hero/hero.component.ts
+++ apps/web/src/app/domain/pages/home/sections/hero/hero.component.ts
@@ -10,7 +10,7 @@ import { HeroDiagramComponent } from './components/hero-diagram.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterModule, HeroDiagramComponent, ZardButtonComponent],
   template: `
-    <div class="relative z-2 mb-0 overflow-hidden md:mb-15">
+    <div class="relative z-2 mb-0 overflow-hidden">
       <div class="container flex flex-col items-center gap-2 py-8 text-center md:py-16 lg:py-20 xl:gap-4">
         <div class="mx-auto mt-25 max-w-4xl space-y-8 text-center">
           <h1
